7365203e7b5491d0179efdb4691d561ff72553ac
xulihang <xulihanghai@163.com>
2024-01-09 17:48:16 +0800

    add opencv document detect handler for DDV

---
+++ src/dynamsoft-document-viewer-handler.ts
@@ -0,0 +1,41 @@
+import { DocumentScanner } from ".";
+
+// Inherit DocumentDetect class
+const Dynamsoft = (window as any)["Dynamsoft"];
+export class OpenCVDocumentDetectHandler extends Dynamsoft.DDV.DocumentDetect {
+  private documentScanner:DocumentScanner;
+  constructor(documentScanner:DocumentScanner){
+    super();
+    this.documentScanner = documentScanner;
+  }
+  // Rewrite the detect method
+  async detect(image:any, detectConfig:any) {
+    const width = image.width;
+    const height = image.height;
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext('2d');
+    let quad:any[] = [];
+    if (context) {
+      const imageData = context.createImageData(width, height);
+      var pixelData = imageData.data;
+      var inputArrayBuffer = image.data;
+      var inputUint8Array = new Uint8Array(inputArrayBuffer);
+      pixelData.set(inputUint8Array);
+      context.putImageData(imageData, 0, 0);
+      const points = this.documentScanner.detect(canvas as any);
+      points.forEach(p => {
+          quad.push([p.x, p.y]);
+      });
+    }
+    const result:any = {
+      location: quad,
+      width: image.width,
+      height: image.height,
+      config: detectConfig,
+    };
+    const ret = this.processDetectResult(result);
+    return Promise.resolve(ret);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import { DocumentScanner } from "./document-scanner";
+import { OpenCVDocumentDetectHandler } from "./dynamsoft-document-viewer-handler";
 
 (window as any)["DocumentScanner"] = DocumentScanner;
+(window as any)["OpenCVDocumentDetectHandler"] = OpenCVDocumentDetectHandler;
