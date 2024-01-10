91b94fd80bc3ed2d7e9f380e1cb48a54aed95d19
xulihang <xulihanghai@163.com>
2024-01-10 11:01:43 +0800

    handle errors in OpenCVDocumentDetectHandler

--- src/dynamsoft-document-viewer-handler.ts
+++ src/dynamsoft-document-viewer-handler.ts
@@ -10,24 +10,32 @@ export class OpenCVDocumentDetectHandler extends Dynamsoft.DDV.DocumentDetect {
   }
   // Rewrite the detect method
   async detect(image:any, detectConfig:any) {
-    const width = image.width;
-    const height = image.height;
-    const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext('2d');
-    let quad:any[] = [];
-    if (context) {
-      const imageData = context.createImageData(width, height);
-      var pixelData = imageData.data;
-      var inputArrayBuffer = image.data;
-      var inputUint8Array = new Uint8Array(inputArrayBuffer);
-      pixelData.set(inputUint8Array);
-      context.putImageData(imageData, 0, 0);
-      const points = this.documentScanner.detect(canvas as any);
-      points.forEach(p => {
+    const quad:any[] = [];
+    try {
+      const width = image.width;
+      const height = image.height;
+      const canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      const context = canvas.getContext('2d');
+      if (context) {
+        const imageData = context.createImageData(width, height);
+        var pixelData = imageData.data;
+        var inputArrayBuffer = image.data;
+        var inputUint8Array = new Uint8Array(inputArrayBuffer);
+        pixelData.set(inputUint8Array);
+        context.putImageData(imageData, 0, 0);
+        const points = this.documentScanner.detect(canvas as any);
+        points.forEach(p => {
           quad.push([p.x, p.y]);
-      });
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      quad.push([0, 0]);
+      quad.push([0, 0]);
+      quad.push([0, 0]);
+      quad.push([0, 0]);
     }
     const result:any = {
       location: quad,
